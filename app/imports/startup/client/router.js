15a39c39c41950aeb3fe431ee22e7bc6dac9cb54
christianleandro <cleandro@hawaii.edu>
2017-03-22 16:00:58 -1000

    Completed mockups for landing page, login page, and home page

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -2,30 +2,37 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
+  name: 'Landing_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Landing_Page' });
+  },
+});
+
+FlowRouter.route('/login', {
+  name: 'Login_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Login_Page' });
+  },
+});
+
+FlowRouter.route('/home', {
   name: 'Home_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Home_Page' });
   },
 });
 
-FlowRouter.route('/list', {
-  name: 'List_Stuff_Page',
+FlowRouter.route('/browse-clubs', {
+  name: 'Browse_Clubs',
   action() {
-    BlazeLayout.render('App_Body', { main: 'List_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Browse_Clubs' });
   },
 });
 
-FlowRouter.route('/add', {
-  name: 'Add_Stuff_Page',
+FlowRouter.route('/create-club', {
+  name: 'Create_Club',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Add_Stuff_Page' });
-  },
-});
-
-FlowRouter.route('/stuff/:_id', {
-  name: 'Edit_Stuff_Page',
-  action() {
-    BlazeLayout.render('App_Body', { main: 'Edit_Stuff_Page' });
+    BlazeLayout.render('App_Body', { main: 'Create_Club' });
   },
 });
 
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,10 +1,14 @@
-import './add-stuff-page.html';
-import './add-stuff-page.js';
+import './landing-page.html';
+import './landing-page.js';
 
-import './edit-stuff-page.html';
-import './edit-stuff-page.js';
+import './login-page.html';
+import './login-page.js';
+
+import './browse-clubs.html';
+import './browse-clubs.js';
 
 import './home-page.html';
+import './home-page.js';
 
-import './list-stuff-page.html';
-import './list-stuff-page.js';
+import './create-club.html';
+import './create-club';
